8740a07b2cd8f90855502716f9704448d991a935
Simen A. W. Olsen <so@bjerk.io>
2023-04-07 23:14:21 +0200

    feat: add support for repository and workflow dispatch

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,32 @@
-import { info } from '@actions/core';
+import { info, setOutput } from '@actions/core';
 import * as github from '@actions/github';
-// import { config } from './config';
 
 if (github.context.eventName === 'repository_dispatch') {
-  info(JSON.stringify(github.context.payload, null, 2));
-}
+  info('Recognized repository_dispatch event, outputing client_payload');
 
-if (github.context.eventName === 'workflow_dispatch') {
-  info(JSON.stringify(github.context.payload.inputs, null, 2));
+  setOutput('payload', github.context.payload.client_payload);
+
+  // Check if client_payload is an object
+  if (typeof github.context.payload.client_payload === 'object') {
+    // Set outputs
+    for (const [key, value] of Object.entries(
+      github.context.payload.client_payload,
+    )) {
+      setOutput(key, value);
+    }
+  }
+} else if (github.context.eventName === 'workflow_dispatch') {
+  info('Recognized workflow_dispatch event, outputing inputs');
+
+  setOutput('payload', github.context.payload.inputs);
+
+  // Check if inputs is an object
+  if (typeof github.context.payload.inputs === 'object') {
+    // Set outputs
+    for (const [key, value] of Object.entries(github.context.payload.inputs)) {
+      setOutput(key, value);
+    }
+  }
+} else {
+  info('Event not supported');
 }
